49d0a539614cbd88849766cc403234da87bfd49f
Hakob_Tonoyan <hakobtono@gmail.com>
2022-11-23 21:48:41 +0400

    Creating dummy controller

--- src/main/java/com/epam/emotionalhelp/EmotionalHelpApplication.java
+++ src/main/java/com/epam/emotionalhelp/EmotionalHelpApplication.java
@@ -1,2 +1,12 @@
-package com.epam.emotionalhelp;public class EmotionalHelpApplication {
+package com.epam.emotionalhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionalHelpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionalHelpApplication.class, args);
+
+    }
 }
--- src/main/java/com/epam/emotionalhelp/controller/HalloController.java
+++ src/main/java/com/epam/emotionalhelp/controller/HalloController.java
@@ -1,2 +1,14 @@
-package com.epam.emotionalhelp.controller;public class HalloController {
+package com.epam.emotionalhelp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HalloController {
+    @GetMapping("/hallo")
+    public String hallo(){
+        return "Hallo World";
+    }
+
+
 }
