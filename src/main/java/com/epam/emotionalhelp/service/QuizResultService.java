b35efc957d80d7eb50454de173107cbd2d6efef2
Roman Kolesnyk <romakoles1@ukr.net>
2022-11-26 20:34:07 +0200

    32 - add QuizResultController

--- src/main/java/com/epam/emotionalhelp/service/QuizResultService.java
+++ src/main/java/com/epam/emotionalhelp/service/QuizResultService.java
@@ -1,2 +1,10 @@
-package com.epam.emotionalhelp.service;public interface QuizResultService {
+package com.epam.emotionalhelp.service;
+
+import com.epam.emotionalhelp.dto.EmotionDto;
+
+import java.util.List;
+import java.util.Map;
+
+public interface QuizResultService {
+    Map<Long, Integer> calculateResult(List<EmotionDto> emotions);
 }
--- src/main/java/com/epam/emotionalhelp/service/impl/QuizResultServiceImpl.java
+++ src/main/java/com/epam/emotionalhelp/service/impl/QuizResultServiceImpl.java
@@ -1,2 +1,20 @@
-package com.epam.emotionalhelp.service.impl;public class QuizResultServiceImpl {
+package com.epam.emotionalhelp.service.impl;
+
+import com.epam.emotionalhelp.dto.EmotionDto;
+import com.epam.emotionalhelp.service.QuizResultService;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class QuizResultServiceImpl implements QuizResultService {
+    @Override
+    public Map<Long, Integer> calculateResult(List<EmotionDto> emotions) {
+        return emotions.stream()
+                .collect(Collectors.groupingBy(EmotionDto::getEmotionId, LinkedHashMap::new,
+                        Collectors.summingInt(EmotionDto::getValue)));
+    }
 }
