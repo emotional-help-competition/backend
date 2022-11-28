8e04111b5310f29b3bca6da214ffe35fa36e3796
Oleg <pakolegdev@gmail.com>
2022-11-28 22:30:29 +0300

    add UtilityClass annotation

--- src/main/java/com/epam/emotionalhelp/controller/util/EndpointName.java
+++ src/main/java/com/epam/emotionalhelp/controller/util/EndpointName.java
@@ -1,10 +1,7 @@
 package com.epam.emotionalhelp.controller.util;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.experimental.UtilityClass;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @UtilityClass
 public class EndpointName {
     public static final String EMOTIONS = "/v1/emotions";
