056a8cd8a734b8f1b151339431bf653379e9daaa
Hakob_Tonoyan <hakobtono@gmail.com>
2022-11-29 19:39:45 +0400

    Creating CRUD operations.

--- src/main/java/com/epam/emotionalhelp/controller/util/EndpointName.java
+++ src/main/java/com/epam/emotionalhelp/controller/util/EndpointName.java
@@ -7,6 +7,8 @@ public class EndpointName {
     public static final String EMOTIONS = "/v1/emotions";
     public static final String QUESTIONS = "/v1/questions";
     public static final String QUIZ_RESULTS = "/v1/results";
+
+    public static final String QUIZ = "/v1/quizzes";
 }
 
 
--- src/main/java/com/epam/emotionalhelp/repository/QuestionRepository.java
+++ src/main/java/com/epam/emotionalhelp/repository/QuestionRepository.java
@@ -1,9 +1,19 @@
 package com.epam.emotionalhelp.repository;
 
 import com.epam.emotionalhelp.model.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Query(
+            value =
+                    "SELECT * FROM question WHERE "
+                            + "(?1 IS NULL OR LOWER(text) LIKE LOWER(CONCAT('%', ?1, '%'))) ",
+            nativeQuery = true)
+    Page<Question> findAll(String text, Pageable pageable);
 }
