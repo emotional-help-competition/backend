a862eeeed46371325f31cb3cecdaecca5e8d8f86
Oleg <pakolegdev@gmail.com>
2022-12-02 16:45:55 +0300

    add generation Documentation Using @Operation and @ApiResponses

--- src/main/java/com/epam/emotionalhelp/controller/QuizController.java
+++ src/main/java/com/epam/emotionalhelp/controller/QuizController.java
@@ -3,7 +3,13 @@ package com.epam.emotionalhelp.controller;
 import com.epam.emotionalhelp.controller.config.CORSConfig;
 import com.epam.emotionalhelp.controller.dto.QuizRequestDto;
 import com.epam.emotionalhelp.controller.dto.QuizResponseDto;
+import com.epam.emotionalhelp.model.Emotion;
 import com.epam.emotionalhelp.service.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,27 +33,67 @@ import static com.epam.emotionalhelp.controller.util.EndpointName.QUIZ;
 @RequiredArgsConstructor
 public class QuizController {
     private final QuizService quizService;
+
+    @Operation(summary = "Get list of quizzes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Received list of quizzes",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Emotion.class))}),
+            @ApiResponse(responseCode = "500", description = "Error on the server",
+                    content = @Content)})
     @GetMapping
     public Page<QuizResponseDto> findAll(Pageable pageable) {
         return quizService.findAll(pageable);
     }
 
+    @Operation(summary = "Get quiz by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Received the quiz",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Emotion.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Quiz not found",
+                    content = @Content)})
     @GetMapping("/{id}")
     public QuizResponseDto findById(@PathVariable Long id) {
         return quizService.findById(id);
     }
 
+    @Operation(summary = "Create the quiz")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quiz created successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Emotion.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters provided",
+                    content = @Content)})
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public QuizResponseDto create(@RequestBody QuizRequestDto quizRequestDto) {
         return quizService.create(quizRequestDto);
     }
 
+    @Operation(summary = "Update an quiz")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quiz updated successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Emotion.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters provided",
+                    content = @Content),
+    })
     @PatchMapping("/{id}")
     public QuizResponseDto update(@PathVariable Long id, @RequestBody QuizRequestDto quizRequestDto) {
         return quizService.update(id, quizRequestDto);
     }
 
+    @Operation(summary = "Delete an quiz")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Quiz deleted successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Emotion.class))}),
+            @ApiResponse(responseCode = "404", description = "Invalid id supplied",
+                    content = @Content),
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
