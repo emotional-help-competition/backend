ba4ece55bbdcc6bfd202cc1f60bb4fba225f6d55
Maksim Elyashevich <maximelyashevich@gmail.com>
2022-12-05 23:59:43 +0300

    Update CORSConfig.java

--- src/main/java/com/epam/emotionalhelp/controller/config/CORSConfig.java
+++ src/main/java/com/epam/emotionalhelp/controller/config/CORSConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
