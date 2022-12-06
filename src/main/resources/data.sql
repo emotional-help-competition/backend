249508058d578db41e30fb3b618fe240c349c613
Oleg <pakolegdev@gmail.com>
2022-12-06 23:05:36 +0300

    insert new data into data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,13 @@
 INSERT INTO emotion (id, description)
-VALUES (1, 'shocked'),
-       (2, 'stunned'),
-       (3, 'amazed'),
-       (4, 'hatred'),
-       (5, 'lonely'),
-       (6, 'annoyed');
+VALUES (1, 'anger'),
+       (2, 'fear'),
+       (3, 'sadness'),
+       (4, 'love'),
+       (5, 'surprise'),
+       (6, 'joy'),
+       (7, 'accepted'),
+       (8, 'afraid'),
+       (9, 'scared');
 
 
 INSERT INTO question (text, emotion_id)
@@ -31,12 +34,57 @@ VALUES (1, 1),
        (2, 5);
 
 
-INSERT INTO category(emotion_id, description, weight)
-VALUES (5, 'sadness', 0.5),
-       (1, 'surprise', 0.5);
+INSERT INTO subcategory(emotion_id, description, weight)
+VALUES (1, 'rabies', 1),
+       (1, 'fury', 2),
+       (1, 'hate', 3),
+       (1, 'hysteria', 4),
+       (1, 'irritation', 5),
 
-INSERT INTO quiz_attempt(created_at)
-VALUES ('2022-10-15 11:15:10');
+       (2, 'despair', 2),
+       (2, 'fright', 3),
+       (2, 'numbness', 4),
+       (2, 'suspicion', 5),
+       (2, 'anxiety', 5),
 
-INSERT INTO quiz_result(quiz_id, attempt_id, emotion_id, score)
-VALUES (1, 1, 1, 3)
+       (3, 'bitterness', 1),
+       (3, 'yearning', 2),
+       (3, 'sorrow', 3),
+       (3, 'laziness', 4),
+       (3, 'pity', 5),
+
+       (4, 'tenderness', 1),
+       (4, 'heat', 2),
+       (4, 'sympathy', 3),
+       (4, 'bliss', 4),
+       (4, 'confidence', 5),
+
+       (5, 'stunned', 1),
+       (5, 'confused', 2),
+       (5, 'amazed', 3),
+       (5, 'speechless', 4),
+       (5, 'shocked', 5),
+
+       (6, 'proud', 1),
+       (6, 'optimistic', 2),
+       (6, 'happy', 3),
+       (6, 'cheerful', 4),
+       (6, 'enthusiastic', 5),
+
+       (7, 'loved', 1),
+       (7, 'honored', 2),
+       (7, 'appreciated', 3),
+       (7, 'respected', 4),
+       (7, 'brave', 5),
+
+       (8, 'stressed', 1),
+       (8, 'overwhelmed', 2),
+       (8, 'powerless', 3),
+       (8, 'anxious', 4),
+       (8, 'guilty', 5),
+
+       (9, 'helpless', 1),
+       (9, 'rejected', 2),
+       (9, 'insecure', 3),
+       (9, 'submissive', 4),
+       (9, 'embarrassed', 5);
