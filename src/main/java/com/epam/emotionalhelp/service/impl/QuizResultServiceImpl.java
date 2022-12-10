3a228969952039f57aa64552f7ff5b02f1ebacf2
Oleg <pakolegdev@gmail.com>
2022-12-10 12:19:14 +0300

    fix signature of the method for QuizResultRepository

--- src/main/java/com/epam/emotionalhelp/service/impl/QuizResultServiceImpl.java
+++ src/main/java/com/epam/emotionalhelp/service/impl/QuizResultServiceImpl.java
@@ -75,7 +75,7 @@ public class QuizResultServiceImpl implements QuizResultService {
     @Override
     public List<EmotionalMapDto> findQuizResultsByAttemptId(Long attemptId) {
         //Find QuizResult(Emotion , Overall score)
-        var list = quizResultRepository.findQuizResultByAttemptId(attemptId);
+        var list = quizResultRepository.findAllByAttemptId(attemptId);
         //Create list to return
         var resultList = new ArrayList<EmotionalMapDto>();
         //Extract Emotion and get Subcategories based on '%'
