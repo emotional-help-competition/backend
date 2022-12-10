a721460f7eadaa60236cb0340edf4bd91d9b60dc
Oleg <pakolegdev@gmail.com>
2022-12-10 13:58:39 +0300

    add RequiredArgsConstructor and NoArgsConstructor

--- src/main/java/com/epam/emotionalhelp/controller/dto/EmotionDto.java
+++ src/main/java/com/epam/emotionalhelp/controller/dto/EmotionDto.java
@@ -1,11 +1,15 @@
 package com.epam.emotionalhelp.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class EmotionDto {
     @NotNull
