5736a927cec52d352ae9e262bf8473bfb2e66b72
Oleg <89264247+GeloPakDev@users.noreply.github.com>
2022-12-12 15:13:52 +0300

    Update src/main/java/com/epam/emotionalhelp/controller/dto/SubcategoryContainerDto.java
    Co-authored-by: Maksim Elyashevich <maximelyashevich@gmail.com>

--- src/main/java/com/epam/emotionalhelp/controller/dto/SubcategoryContainerDto.java
+++ src/main/java/com/epam/emotionalhelp/controller/dto/SubcategoryContainerDto.java
@@ -5,9 +5,15 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
+/**
+ * This class can be used to store emotions subcategory details.
+ *
+ * @see SubcategoryDto
+ * @deprecated to be removed in future releases
+ */
 @Data
 @RequiredArgsConstructor
-@Deprecated
+@Deprecated(since = "1.0", forRemoval = true)
 public class SubcategoryContainerDto {
     private final Set<String> emotions;
     private final Double score;
